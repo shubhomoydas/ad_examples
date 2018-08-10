5f61d3783f8c9eb82f3a9b395cbadd326e1fc0a2
shubhomoydas <smd.shubhomoydas@gmail.com>
2018-08-10 16:40:40 -0700

    elaborated anomaly detector vs classifier

--- python/aad/anomaly_vs_classifier.py
+++ python/aad/anomaly_vs_classifier.py
@@ -2,17 +2,20 @@ from common.data_plotter import *
 from common.gen_samples import *
 
 from aad.aad_support import *
+from aad.demo_aad import describe_instances
+from classifier_trees import *
 
 """
-pythonw -m aad.anomaly_vs_classifier
+pythonw -m aad.anomaly_vs_classifier --dataset=5 --algo=explain
 """
 
 logger = logging.getLogger(__name__)
 
 
-def get_debug_args(detector_type=AAD_IFOREST):
+def get_debug_args(dataset="", detector_type=AAD_IFOREST):
     return ["--resultsdir=./temp", "--randseed=42",
             "--reruns=1",
+            "--dataset=%s" % dataset,
             "--detector_type=%d" % detector_type,
             "--forest_score_type=%d" %
             (IFOR_SCORE_TYPE_NEG_PATH_LEN if detector_type == AAD_IFOREST
@@ -30,6 +33,7 @@ def get_debug_args(detector_type=AAD_IFOREST):
             "--forest_max_depth=%d" % (100 if detector_type == AAD_IFOREST else 7),
             "--forest_add_leaf_nodes_only",
             "--ensemble_score=%d" % ENSEMBLE_SCORE_LINEAR,
+            "--describe_n_top=5",
             "--resultsdir=./temp",
             "--log_file=./temp/anomaly_vs_classifier.log",
             "--debug"]
@@ -41,7 +45,14 @@ def get_auc(model, x, y, x_transformed=None):
     return auc
 
 
-def train_anomaly_detector(x, y, opts, test_points):
+def plot_regions(model, region_indexes, pl):
+    axis_lims = (plt.xlim(), plt.ylim())
+    for i in region_indexes:
+        region = model.all_regions[i].region
+        plot_rect_region(pl, region, "red", axis_lims)
+
+
+def train_anomaly_detector(x, y, opts, test_points, name, explain=False):
     rng = np.random.RandomState(opts.randseed)
 
     # fit the model
@@ -74,7 +85,7 @@ def train_anomaly_detector(x, y, opts, test_points):
     logger.debug("best_i: %d, best_auc: %f" % (best_i, best_auc))
     model.w = best_w
 
-    pdfpath = "%s/%s.pdf" % (opts.resultsdir, "avc_train_anomaly")
+    pdfpath = "%s/%s.pdf" % (opts.resultsdir, "%s_anomaly" % name)
     logger.debug("Plotting aad contours to %s" % (pdfpath))
     dp = DataPlotter(pdfpath=pdfpath, rows=1, cols=1)
     pl = dp.get_next_plot()
@@ -103,8 +114,16 @@ def train_anomaly_detector(x, y, opts, test_points):
 
     dp.close()
 
+    if explain:
+        ridxs_counts, region_extents = describe_instances(x, np.array(ha), model=model, opts=opts)
+        logger.debug("selected region indexes and corresponding instance counts (among %d):\n%s" %
+                     (len(ha), str(list(ridxs_counts))))
+        region_indexes = [region for region, count in ridxs_counts]
+        plot_dataset(x, y, opts, test_points=None, name="%s_anomaly_descriptions" % name,
+                     model=model, region_indexes=region_indexes, legend=False)
 
-def train_classifier(x, y, opts, test_points):
+
+def train_classifier(x, y, opts, test_points, name, explain=False):
     classifier = RFClassifier.fit(x, y, n_estimators=100, max_depth=None)
 
     xx, yy = np.meshgrid(np.linspace(-4, 8, 50), np.linspace(-4, 8, 50))
@@ -112,7 +131,7 @@ def train_classifier(x, y, opts, test_points):
     probs_grid = classifier.predict_prob_for_class(x_test, 1)
     Z = probs_grid.reshape(xx.shape)
 
-    pdfpath = "%s/%s.pdf" % (opts.resultsdir, "avc_train_classifier")
+    pdfpath = "%s/%s.pdf" % (opts.resultsdir, "%s_classifier" % name)
     logger.debug("Plotting classifier contours to %s" % (pdfpath))
     dp = DataPlotter(pdfpath=pdfpath, rows=1, cols=1)
     pl = dp.get_next_plot()
@@ -135,9 +154,33 @@ def train_classifier(x, y, opts, test_points):
 
     dp.close()
 
+    if explain:
+        # generate compact descriptions from the Random Forest classifier
+        rfre = RandomForestRegionExtractor(x, y, classifier.clf)
+        ha = np.where(y == 1)[0]
+        ridxs_counts, region_extents = describe_instances(x, ha, model=rfre, opts=opts)
+        logger.debug("selected random forest region indexes and corresponding instance counts (among %d):\n%s" %
+                     (len(ha), str(list(ridxs_counts))))
+        region_indexes = [region for region, count in ridxs_counts]
+        plot_dataset(x, y, opts, test_points=None, name="%s_random_forest_descriptions" % name,
+                     model=rfre, region_indexes=region_indexes, legend=False)
 
-def plot_dataset(x, y, opts, test_points):
-    pdfpath = "%s/%s.pdf" % (opts.resultsdir, "avc_dataset")
+
+def plot_decision_tree_descriptions(x, y, name):
+    ha = np.where(y == 1)[0]
+    # generate compact descriptions from the Decision Tree classifier;
+    # these just correspond to the rules extracted from the tree structure
+    dt = DecisionTreeRegionExtractor(x, y)
+    ridxs_counts, region_extents = describe_instances(x, ha, model=dt, opts=opts)
+    logger.debug("selected decision tree region indexes and corresponding instance counts (among %d):\n%s" %
+                 (len(ha), str(list(ridxs_counts))))
+    region_indexes = [region for region, count in ridxs_counts]
+    plot_dataset(x, y, opts, test_points=None, name="%s_decision_tree_descriptions" % name,
+                 model=dt, region_indexes=region_indexes, legend=False)
+
+
+def plot_dataset(x, y, opts, test_points=None, name=None, model=None, region_indexes=None, legend=True):
+    pdfpath = "%s/%s.pdf" % (opts.resultsdir, name)
     logger.debug("Plotting dataset to %s" % (pdfpath))
     dp = DataPlotter(pdfpath=pdfpath, rows=1, cols=1)
     pl = dp.get_next_plot()
@@ -145,25 +188,45 @@ def plot_dataset(x, y, opts, test_points):
     anoms = np.where(y == 1)[0]
     pl.scatter(x[nomls, 0], x[nomls, 1], marker='x', s=25, facecolors='blue', edgecolors="blue", label="Nominal")
     pl.scatter(x[anoms, 0], x[anoms, 1], marker='x', s=25, facecolors='red', edgecolors="red", label="Anomaly")
-    pl.scatter(test_points[:, 0], test_points[:, 1], marker='x', s=50, facecolors='green', edgecolors="green", label="Unknown", linewidths=2.)
-    pl.scatter(test_points[:, 0], test_points[:, 1], marker='o', s=60, facecolors='none', edgecolors="green", label=None)
-    pl.legend(loc='lower right', prop={'size': 14})
+
+    if test_points is not None:
+        pl.scatter(test_points[:, 0], test_points[:, 1], marker='x', s=50, facecolors='green', edgecolors="green", label="Unknown", linewidths=2.)
+        pl.scatter(test_points[:, 0], test_points[:, 1], marker='o', s=60, facecolors='none', edgecolors="green", label=None)
+
+    if legend:
+        pl.legend(loc='lower right', prop={'size': 14})
+
+    if model is not None and region_indexes is not None:
+        plot_regions(model, region_indexes, pl)
+
     dp.close()
 
 
 if __name__ == "__main__":
-    args = get_aad_command_args(debug=True, debug_args=get_debug_args())
-    # print "log file: %s" % args.log_file
-    configure_logger(args)
+    # the main program arguments from commandline
+    args = get_command_args(debug=False)
+    explain = args.algo == "explain"
 
-    opts = AadOpts(args)
+    # the AAD arguments
+    aadArgs = get_aad_command_args(debug=True,
+                                   debug_args=get_debug_args(dataset=args.dataset))
+    # print "log file: %s" % aadArgs.log_file
+    configure_logger(aadArgs)
+
+    opts = AadOpts(aadArgs)
     logger.debug(opts.str_opts())
 
     np.random.seed(opts.randseed)
 
-    x, y = get_synthetic_samples(stype=5)
+    synthetic_dataset_id = int(args.dataset)
+    x, y = get_synthetic_samples(stype=synthetic_dataset_id)
     test_points = np.array([-3., 7.], dtype=np.float32).reshape((1,2))
 
-    plot_dataset(x, y, opts, test_points)
-    train_anomaly_detector(x, y, opts, test_points)
-    train_classifier(x, y, opts, test_points)
+    name = "avc_dataset_%d" % synthetic_dataset_id
+    plot_dataset(x, y, opts, test_points=test_points, name=name)
+    train_anomaly_detector(x, y, opts, test_points, name=name, explain=explain)
+    train_classifier(x, y, opts, test_points, name=name, explain=explain)
+
+    # plot rules from a decision tree classifier
+    if explain:
+        plot_decision_tree_descriptions(x, y, name=name)
---
+++ python/aad/classifier_trees.py
@@ -0,0 +1,116 @@
+from aad.aad_support import *
+
+
+class DTForest(object):
+    def __init__(self, tree):
+        self.estimators_ = [tree]
+
+
+class DecisionTreeRegionExtractor(AadForest):
+    """ Extracts regions from a decision tree classifier which cover selected instances
+
+    Since much of the functionality is already in AadForest, we merely initialize the
+    internal structures in the constructor. Later, the requisite functions in AadForest
+    will be invoked.
+    """
+
+    def __init__(self,
+                 x, y,
+                 max_depth=10,
+                 score_type=IFOR_SCORE_TYPE_CONST,
+                 ensemble_score=ENSEMBLE_SCORE_LINEAR,
+                 random_state=None,
+                 detector_type=AAD_IFOREST):
+        Aad.__init__(self, detector_type, ensemble_score, random_state)
+
+        self.max_depth = max_depth
+        self.n_estimators = 1
+        self.max_samples = x.shape[0]
+        self.tree_update_type = TREE_UPD_OVERWRITE
+        self.tree_incremental_update_weight = None
+        self.forest_replace_frac = None
+        self.feature_partitions = None
+
+        self.score_type = score_type
+
+        self.add_leaf_nodes_only = True
+
+        # store all regions grouped by tree
+        self.regions_in_forest = None
+
+        # store all regions in a flattened list (ungrouped)
+        self.all_regions = None
+
+        # store maps of node index to region index for all trees
+        self.all_node_regions = None
+
+        # scores for each region
+        self.d = None
+
+        self.decision_tree = DTClassifier.fit(x, y, self.max_depth)
+        self.clf = DTForest(self.decision_tree.clf)
+        self._init_structures()
+        self.init_weights(init_type=INIT_UNIF)
+
+    def _init_structures(self):
+        self.regions_in_forest = []
+        self.all_regions = []
+        self.all_node_regions = []
+        region_id = 0
+        for i in range(len(self.clf.estimators_)):
+            regions = self.extract_leaf_regions_from_tree(self.clf.estimators_[i],
+                                                          self.add_leaf_nodes_only)
+            self.regions_in_forest.append(regions)
+            self.all_regions.extend(regions)
+            node_regions = {}
+            for region in regions:
+                node_regions[region.node_id] = region_id
+                region_id += 1  # this will monotonously increase across trees
+            self.all_node_regions.append(node_regions)
+            # print "%d, #nodes: %d" % (i, len(regions))
+        self.d, _, _ = self.get_region_scores(self.all_regions)
+        # self.w = self.get_uniform_weights()
+        self.w_unif_prior = self.get_uniform_weights()
+
+    def fit(self, x):
+        raise NotImplementedError("fit() not implemented for DecisionTreeRegionExtractor")
+
+
+class RandomForestRegionExtractor(DecisionTreeRegionExtractor):
+
+    def __init__(self, x, y, clf, score_type=IFOR_SCORE_TYPE_CONST,
+                 ensemble_score=ENSEMBLE_SCORE_LINEAR,
+                 random_state=None,
+                 detector_type=AAD_IFOREST):
+        Aad.__init__(self, detector_type, ensemble_score, random_state)
+
+        self.max_depth = clf.max_depth
+        self.n_estimators = 1
+        self.max_samples = x.shape[0]
+        self.tree_update_type = TREE_UPD_OVERWRITE
+        self.tree_incremental_update_weight = None
+        self.forest_replace_frac = None
+        self.feature_partitions = None
+
+        self.score_type = score_type
+
+        self.add_leaf_nodes_only = True
+
+        # store all regions grouped by tree
+        self.regions_in_forest = None
+
+        # store all regions in a flattened list (ungrouped)
+        self.all_regions = None
+
+        # store maps of node index to region index for all trees
+        self.all_node_regions = None
+
+        # scores for each region
+        self.d = None
+
+        self.clf = clf
+        self._init_structures()
+        self.init_weights(init_type=INIT_UNIF)
+
+    def fit(self, x):
+        raise NotImplementedError("fit() not implemented for RandomForestRegionExtractor")
